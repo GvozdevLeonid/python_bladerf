1acb0c9794b251661cb1cb9c27e056015915fd8f
Gvozdev Leonid <gvozdev-leonid-95@yandex.ru>
2025-06-09 17:07:38 +0200

    cleaned up the code

--- android/libusb/__init__.py
+++ android/libusb/__init__.py
@@ -1,32 +1,37 @@
+# ruff: noqa: RUF012
 import os
 import shutil
-from typing import Any
+from typing import Any, override
 
-import sh  # type: ignore
-from pythonforandroid.archs import Arch  # type: ignore
-from pythonforandroid.logger import shprint  # type: ignore
-from pythonforandroid.recipe import NDKRecipe  # type: ignore
-from pythonforandroid.util import current_directory  # type: ignore
+import sh
+from pythonforandroid.archs import Arch
+from pythonforandroid.logger import shprint
+from pythonforandroid.recipe import NDKRecipe
+from pythonforandroid.util import current_directory
 
 
-class LibusbRecipe(NDKRecipe):  # type: ignore
+class LibusbRecipe(NDKRecipe):
 
     url = 'https://github.com/libusb/libusb/archive/refs/tags/v{version}.tar.gz'
-    generated_libraries = ('libusb-1.0.so', )
+    generated_libraries = ['libusb-1.0.so']
     site_packages_name = 'libusb'
     version = '1.0.26'
     name = 'libusb'
 
+    @override
     def should_build(self, arch: Arch) -> bool:
         return not os.path.exists(os.path.join(self.ctx.get_libs_dir(arch.arch), 'libusb1.0.so'))
 
+    @override
     def get_jni_dir(self, arch: Arch) -> str:
         return os.path.join(self.get_build_dir(arch.arch), 'android', 'jni')
 
+    @override
     def get_lib_dir(self, arch: Arch) -> str:
         return os.path.join(self.get_build_dir(arch.arch), 'android', 'obj', 'local', arch.arch)
 
-    def build_arch(self, arch: Arch, *extra_args: Any) -> None:
+    @override
+    def build_arch(self, arch: Arch, *args: Any) -> None:
         env = self.get_recipe_env(arch)
         with current_directory(self.get_build_dir(arch.arch)):
             shprint(
@@ -35,7 +40,7 @@ class LibusbRecipe(NDKRecipe):  # type: ignore
                 'APP_PLATFORM=android-' + str(self.ctx.ndk_api),
                 'NDK=' + self.ctx.ndk_dir,
                 'APP_ABI=' + arch.arch,
-                *extra_args,
+                *args,
                 _env=env,
             )
 
--- android/python_bladerf/__init__.py
+++ android/python_bladerf/__init__.py
@@ -1,29 +1,35 @@
+# ruff: noqa: RUF012
 import os
-from typing import Any
+from typing import Any, override
 
-from pythonforandroid.archs import Arch  # type: ignore
-from pythonforandroid.recipe import (  # type: ignore
-    CythonRecipe,
+from pythonforandroid.archs import Arch
+from pythonforandroid.recipe import (
+    PyProjectRecipe,
     Recipe,
 )
 
 
-class PythonBladerfRecipe(CythonRecipe):  # type: ignore
+class PythonBladerfRecipe(PyProjectRecipe):
     url = 'https://github.com/GvozdevLeonid/python_bladerf/releases/download/v.{version}/python_bladerf-{version}.tar.gz'
-    depends = ('python3', 'setuptools', 'numpy', 'pyjnius', 'libbladerf')
+    depends = ['python3', 'setuptools', 'numpy', 'pyjnius', 'libbladerf']
+    hostpython_prerequisites = ['Cython>=3.1.0,<3.2']
     site_packages_name = 'python_bladerf'
     name = 'python_bladerf'
-    version = '1.3.1'
+    version = '1.4.0'
 
-    def get_recipe_env(self, arch: Arch) -> dict[str, Any]:
-        env: dict[str, Any] = super().get_recipe_env(arch)
+    @override
+    def get_recipe_env(self, arch: Arch, **kwargs: Any) -> dict[str, Any]:
+        env: dict[str, Any] = super().get_recipe_env(arch, **kwargs)
 
         libbladerf_recipe = Recipe.get_recipe('libbladerf', arch)
-
         libbladerf_h_dir = os.path.join(libbladerf_recipe.get_build_dir(arch), 'host', 'libraries', 'libbladeRF', 'include')
 
-        env['LDFLAGS'] += f' -L{self.ctx.get_libs_dir(arch.arch)}'
-        env['CFLAGS'] += f' -I{libbladerf_h_dir}'
+        env['LDFLAGS'] = env['LDFLAGS'] + f' -L{self.ctx.get_libs_dir(arch.arch)} -lbladeRF'
+        env['CFLAGS'] = env['CFLAGS'] + f' -I{libbladerf_h_dir}'
+
+        env['PYTHON_BLADERF_LIBBLADERF_H_PATH'] = libbladerf_h_dir
+        env['LDSHARED'] = env['CC'] + ' -shared'
+        env['LIBLINK'] = 'NOTNONE'
 
         return env
 
